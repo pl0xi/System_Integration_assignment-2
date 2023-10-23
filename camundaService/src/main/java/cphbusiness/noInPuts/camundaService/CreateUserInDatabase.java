e5050422d5f9fb4e79036888a9133c6ce656aa5f
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-10-23 08:14:02 +0200

    Apache Camel setup

--- camundaService/src/main/java/cphbusiness/noInPuts/camundaService/CreateUserInDatabase.java
+++ camundaService/src/main/java/cphbusiness/noInPuts/camundaService/CreateUserInDatabase.java
@@ -1,12 +1,23 @@
 package cphbusiness.noInPuts.camundaService;
 
 import jakarta.inject.Named;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 @Named
 public class CreateUserInDatabase implements JavaDelegate {
 
         @Override
         public void execute(org.camunda.bpm.engine.delegate.DelegateExecution execution) throws Exception {
-            System.out.println("CreateUserInDatabase");
+            CamelContext context = new DefaultCamelContext();
+            context.start();
+
+            ProducerTemplate producerTemplate = context.createProducerTemplate();
+            try {
+                producerTemplate.sendBody("http://localhost:8081/camel/user/create", "test");
+            } finally {
+                context.stop();
+            }
         }
 }
---
+++ eip/src/main/java/cphbusiness/noInPuts/eip/UserRoute.java
@@ -0,0 +1,21 @@
+package cphbusiness.noInPuts.eip;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRoute extends RouteBuilder {
+
+        @Override
+        public void configure() throws Exception {
+            rest("/user")
+                    .post("/create")
+                    .produces("application/json")
+                    .outType(String.class)
+                    .to("direct:createUser");
+
+            from("direct:createUser")
+                    .log("User is created")
+                    .setBody(simple("User is created"));
+        }
+}
