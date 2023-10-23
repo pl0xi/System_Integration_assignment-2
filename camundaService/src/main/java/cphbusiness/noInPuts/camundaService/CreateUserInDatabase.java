fe6e1bd84f8eda75ab5c450b324e3e9382b1b0bf
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-10-23 08:32:29 +0200

    Camunda Process Engine values -> Apache Camel Middleware

--- camundaService/src/main/java/cphbusiness/noInPuts/camundaService/CreateUserInDatabase.java
+++ camundaService/src/main/java/cphbusiness/noInPuts/camundaService/CreateUserInDatabase.java
@@ -14,8 +14,17 @@ public class CreateUserInDatabase implements JavaDelegate {
             context.start();
 
             ProducerTemplate producerTemplate = context.createProducerTemplate();
+            String name = execution.getVariable("Name").toString();
+            String phoneNumber = execution.getVariable("phone").toString();
+            String email = execution.getVariable("email").toString();
+
+
             try {
-                producerTemplate.sendBody("http://localhost:8081/camel/user/create", "test");
+                producerTemplate.sendBody("http://localhost:8081/camel/user/create", String.format("{" +
+                        "    \"name\": \"%s\"," +
+                        "    \"phone_number\": \"%s\"," +
+                        "    \"email\": \"%s\"" +
+                        "}", name, phoneNumber, email));
             } finally {
                 context.stop();
             }
